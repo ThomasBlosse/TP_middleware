4331a90db06611fe408b9a773ea8929e6146302e
Thomas <thomas.blosse.gilio@gmail.com>
2025-02-03 14:09:38 +0100

    creation model.go

---
+++ API_Timetable/internal/models/models.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"github.com/gofrs/uuid"
+	"time"
+)
+
+type Collection struct {
+	Id      		*uuid.UUID 	`json:"id"`
+	ResourceIds		*uuid.UUID	`json:"resourceIds"`
+	Uid 			string 		`json:"uid"`
+	Description 	string 		`json:"description"`
+	Name 			string 		`json:"name"`
+	Started			time.Time	`json:"started"`
+	End 			time.Time	`json:"end"`
+	Location		str			`json:"Location"`
+	LastUpdate		time.Time	`json:"lastupdate"`
+}
