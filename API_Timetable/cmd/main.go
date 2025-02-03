649ca13d3f3a5b12084869ed56f7e7d5765fb93c
Thomas <thomas.blosse.gilio@gmail.com>
2025-02-03 13:46:48 +0100

    creation main.go, et debut chemin API

---
+++ API_Timetable/cmd/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/sirupsen/logrus"
+	"API_Timetable/internal/controllers/collections"
+	"API_Timetable/internal/helpers"
+	"API_Timetable/internal/models"
+	"net/http"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Route("/collections", func(r chi.Router) {
+		r.Get("/", collections.GetCollections)
+		r.Post("/", collections.CreateCollection)
+
+		r.Route("/{id}", func(r chi.Router) {
+			r.Use(collections.Ctx)
+			r.Get("/", collections.GetCollection)
+			r.Put("/", collections.UpdateCollectionItem)
+			r.Delete("/", collections.DeleteCollectionItem)
+		})
+	})
+
+	logrus.Info("[INFO] Web server started. Now listening on *:8080")
+	logrus.Fatalln(http.ListenAndServe(":8080", r))
+
+}
+
+
+func init() {
+	db, err := helpers.OpenDB()
+	if err != nil {
+		logrus.Fatalf("error while opening database : %s", err.Error())
+	}
+	schemes := []string{
+		`CREATE TABLE IF NOT EXISTS collections (
+			id VARCHAR(255) PRIMARY KEY NOT NULL UNIQUE,
+			content VARCHAR(255) NOT NULL
+		);`,
+	}
+	for _, scheme := range schemes {
+		if _, err := db.Exec(scheme); err != nil {
+			logrus.Fatalln("Could not generate table ! Error was : " + err.Error())
+		}
+	}
+	helpers.CloseDB(db)
+}
