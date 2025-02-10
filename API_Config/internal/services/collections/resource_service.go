c21f075cd96942d08733bbe1afa58ed1273651a5
Mowsoon <alban.munari@gmail.com>
2025-02-10 20:38:47 +0100

    service GetResourceById

--- API_Config/internal/services/collections/resource_service.go
+++ API_Config/internal/services/collections/resource_service.go
@@ -3,11 +3,13 @@ package collections
 import (
 	"API_Config/internal/models\"
 	resources "API_Config/internal/repositories/collections"
+	"database/sql"
+	"github.com/gofrs/uuid"
 	"github.com/sirupsen/logrus"
+	"net/http"
 )
 
 func GetAllResources() ([]models.Resources, error) {
-	var err error
 	resources, err := resources.GetAllResources()
 	if err != nil {
 		logrus.Errorf("error retrieving resources : %s", err.Error())
@@ -18,4 +20,23 @@ func GetAllResources() ([]models.Resources, error) {
 	}
 
 	return resources, nil
+}
+
+func GetResourceById(id uuid.UUID) (*models.Resources, error) {
+	resource, err := resources.GetCollectionById(id)
+	if err != nil {
+		if err.Error() == sql.ErrNoRows.Error() {
+			return nil, &models.CustomError{
+				Message: "resource not found",
+				Code:    http.StatusNotFound,
+			}
+		}
+		logrus.Errorf("error retrieving resources : %s", err.Error())
+		return nil, &models.CustomError{
+			Message: "Something went wrong",
+			Code:    500,
+		}
+	}
+
+	return resource, nil
 }
