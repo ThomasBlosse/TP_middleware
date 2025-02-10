73cf5875dad5aab017fe9fbb280c000eebc58907
Mowsoon <alban.munari@gmail.com>
2025-02-10 17:05:07 +0100

    GetAllAlerts

--- API_Config/internal/repositories/collections/alerts.go
+++ API_Config/internal/repositories/collections/alerts.go
@@ -1,1 +1,31 @@
 package collections
+
+import (
+	"API_Timetable/internal/helpers"
+	"API_Timetable/internal/models"
+)
+
+func GetAllAlerts() ([]models.Alerts, error) {
+	db, err := helpers.OpenDB()
+	if err != nil {
+		return nil, err
+	}
+	rows, err := db.Query("SELECT * FROM alerts")
+	helpers.CloseDB(db)
+	if err != nil {
+		return nil, err
+	}
+
+	alerts := []models.Alerts{}
+	for rows.Next() {
+		var alert models.Alerts
+		err := rows.Scan(&alert.Name, &alert.Uid, &alert.Id)
+		if err != nil {
+			return nil, err
+		}
+		alerts = append(alerts, alert)
+	}
+	_ = rows.Close()
+
+	return alerts, nil
+}
