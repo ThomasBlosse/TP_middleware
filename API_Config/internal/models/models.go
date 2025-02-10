d7d2e2c2a403a47fd8aa99452fec701c7d40e798
Mowsoon <alban.munari@gmail.com>
2025-02-10 16:25:14 +0100

    Alerts

--- API_Config/internal/models/models.go
+++ API_Config/internal/models/models.go
@@ -5,3 +5,9 @@ type Resources struct {
 	Uid  string     `json:"UCA ID"`
 	Id   *uuid.UUID `json:"id"`
 }
+
+type Alerts struct {
+	Email   string      `json:"email"`
+	Targets interface{} `json:"targets"`
+	Id      *uuid.UUID  `json:"id"`
+}
