ceed9b9fc693c114eacbd57e833aab530dfdd894
Mowsoon <alban.munari@gmail.com>
2025-02-10 16:51:30 +0100

    GetResourceById

--- API_Config/internal/repositories/collections/resources.go
+++ API_Config/internal/repositories/collections/resources.go
@@ -29,3 +29,19 @@ func GetAllResources() ([]models.Resources, error) {
 
 	return resources, nil
 }
+
+func GetResourceById(id uuid.UUID) (*models.Resources, error) {
+	db, err := helpers.OpenDB()
+	if err != nil {
+		return nil, err
+	}
+	row := db.QueryRow("SELECT * FROM resources WHERE id=?", id.String())
+	helpers.CloseDB(db)
+
+	var resource models.Resources
+	err := rows.Scan(&resource.Name, &resource.Uid, &resource.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &resource, nil
+}
