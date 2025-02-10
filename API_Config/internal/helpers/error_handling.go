854eb86494f0e34d2b9309be760ccf533e384a20
Mowsoon <alban.munari@gmail.com>
2025-02-10 21:21:28 +0100

    helper adding error_handling for CheckResourceExists to avoid replication of code

---
+++ API_Config/internal/helpers/error_handling.go
@@ -0,0 +1,25 @@
+package helpers
+
+import (
+	"API_Config/internal/models"
+	"API_Config/internal/services/resource_service"
+	"github.com/gofrs/uuid"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
+// CheckResourceExists vérifie si une ressource existe, sinon retourne une erreur CustomError.
+func CheckResourceExists(resourceId uuid.UUID) error {
+	_, err := resource_service.GetResourceById(resourceId)
+	if err != nil {
+		if customErr, ok := err.(*models.CustomError); ok {
+			return customErr
+		}
+		logrus.Errorf("error retrieving resource: %s", err.Error())
+		return &models.CustomError{
+			Message: "Resource not found",
+			Code:    http.StatusNotFound,
+		}
+	}
+	return nil
+}
