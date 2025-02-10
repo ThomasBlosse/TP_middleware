1a634d52e7daf843c8713511a01c236b183ca3da
Mowsoon <alban.munari@gmail.com>
2025-02-10 17:07:30 +0100

    GetAlertById

--- API_Config/internal/repositories/collections/alerts.go
+++ API_Config/internal/repositories/collections/alerts.go
@@ -19,7 +19,7 @@ func GetAllAlerts() ([]models.Alerts, error) {
 	alerts := []models.Alerts{}
 	for rows.Next() {
 		var alert models.Alerts
-		err := rows.Scan(&alert.Name, &alert.Uid, &alert.Id)
+		err := rows.Scan(&alert.Email, &alert.Targets, &alert.Id)
 		if err != nil {
 			return nil, err
 		}
@@ -28,4 +28,20 @@ func GetAllAlerts() ([]models.Alerts, error) {
 	_ = rows.Close()
 
 	return alerts, nil
+}
+
+func GetAlertById(id uuid.UUID) (*models.Alerts, error) {
+	db, err := helpers.OpenDB()
+	if err != nil {
+		return nil, err
+	}
+	row := db.QueryRow("SELECT * FROM alerts WHERE id=?", id.String())
+	helpers.CloseDB(db)
+
+	var alert models.Alerts
+	err := rows.Scan(&alert.Email, &alert.Targets, &alert.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &alert, nil
 }
