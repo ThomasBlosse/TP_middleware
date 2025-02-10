25f86026c4573ef8cf05a215aa7af3b3cf523d17
Mowsoon <alban.munari@gmail.com>
2025-02-10 17:54:19 +0100

    DeleteAlertById and adding CloseDB everywhere

--- API_Config/internal/repositories/collections/alerts.go
+++ API_Config/internal/repositories/collections/alerts.go
@@ -143,5 +143,16 @@ func PutAlert(alertId uuid.UUID, newTargets interface{}) error {
 		string(targetsJSON),
 		alertId.String(),
 	)
+	helpers.CloseDB()
+	return err
+}
+
+func DeleteAlertById(alertId uuid.UUID) error {
+	db, err := helpers.OpenDB()
+	if err != nil {
+		return nil, err
+	}
+	_, err = db.Exec("DELETE FROM resources  WHERE if=?", alertId.String())
+	helpers.CloseDB()
 	return err
 }
