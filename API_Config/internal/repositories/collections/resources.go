78c2baa1a6afa4ea8945bbc392a7beef775eab4f
Mowsoon <alban.munari@gmail.com>
2025-02-10 16:48:57 +0100

    GetAllResources

--- API_Config/internal/repositories/collections/resources.go
+++ API_Config/internal/repositories/collections/resources.go
@@ -1,1 +1,31 @@
 package collections
+
+import (
+	"API_Timetable/internal/helpers"
+	"API_Timetable/internal/models"
+)
+
+func GetAllResources() ([]models.Resources, error) {
+	db, err := helpers.OpenDB()
+	if err != nil {
+		return nil, err
+	}
+	rows, err := db.Query("SELECT * FROM resources")
+	helpers.CloseDB(db)
+	if err != nil {
+		return nil, err
+	}
+
+	resources := []models.Resources{}
+	for rows.Next() {
+		var resource models.Resources
+		err := rows.Scan(&resource.Name, &resource.Uid, &resource.Id)
+		if err != nil {
+			return nil, err
+		}
+		resources = append(resources, resource)
+	}
+	_ = rows.Close()
+
+	return resources, nil
+}
