ae862acc8f9b9d7c6348114fe4a0a1087aa85ae9
Mowsoon <alban.munari@gmail.com>
2025-02-10 20:50:18 +0100

    service GetAllAlerts

--- API_Config/internal/services/collections/alert_service.go
+++ API_Config/internal/services/collections/alert_service.go
@@ -1,1 +1,20 @@
 package collections
+
+import (
+	"API_Config/internal/models\"
+	alerts "API_Config/internal/repositories/collections"
+	"github.com/sirupsen/logrus"
+)
+
+func GetAllAlerts() ([]models.Alerts, error) {
+	allAlerts, err := alerts.GetAllAlerts()
+	if err != nil {
+		logrus.Errorf("error retrieving all the alerts : %s", err.Error())
+		return nil, &models.CustomError{
+			Message: "Something went wrong",
+			Code:    500,
+		}
+	}
+
+	return allAlerts, nil
+}
