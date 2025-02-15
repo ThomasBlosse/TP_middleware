b8e66be61991eb0a048215bc9f0cd872322d6973
Mowsoon <alban.munari@gmail.com>
2025-02-15 16:16:28 +0100

    controllers get_resource.go

---
+++ API_Config/internal/controllers/collections/resources/get_resource.go
@@ -0,0 +1,34 @@
+package resources
+
+import (
+	"API_Config/internal/models"
+	"API_Config/internal/services/collections/resource_service"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gofrs/uuid"
+	"github.com/sirupsen/logrus"
+)
+
+func GetResource(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	collectionId, _ := ctx.Value("collectionId").(uuid.UUID)
+
+	collection, err := resource_service.GetResourceById(collectionId)
+	if err != nil {
+		logrus.Errorf("error : %s", err.Error())
+		customError, isCustom := err.(*models.CustomError)
+		if isCustom {
+			w.WriteHeader(customError.Code)
+			body, _ := json.Marshal(customError)
+			_, _ = w.Write(body)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	body, _ := json.Marshal(collection)
+	_, _ = w.Write(body)
+}
