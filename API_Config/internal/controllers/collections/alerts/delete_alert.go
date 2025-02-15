87d968cbb74b1d605662f59badb016f0f0b24026
Mowsoon <alban.munari@gmail.com>
2025-02-15 16:12:20 +0100

    controllers delete_alert.go

---
+++ API_Config/internal/controllers/collections/alerts/delete_alert.go
@@ -0,0 +1,33 @@
+package alerts
+
+import (
+	"API_Config/internal/models"
+	"API_Config/internal/services/collections/alert_service"
+	"encoding/json"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
+func DeleteAlert(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	collectionId, _ := ctx.Value("collectionId").(uuid.UUID)
+
+	err := alert_service.DeleteAlertById(collectionId)
+	if err != nil {
+		logrus.Errorf("error deleting alert: %s", err.Error())
+
+		customError, isCustom := err.(*models.CustomError)
+		if isCustom {
+			w.WriteHeader(customError.Code)
+			body, _ := json.Marshal(customError)
+			_, _ = w.Write(body)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	body, _ := json.Marshal(map[string]string{"message": "Alert deleted successfully"})
+	_, _ = w.Write(body)
+}
