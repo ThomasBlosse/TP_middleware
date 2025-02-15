b2415d70093b6cd2089d9fd6cea5ff919143af60
Mowsoon <alban.munari@gmail.com>
2025-02-15 16:03:52 +0100

    controllers get_alerts_uid.go

---
+++ API_Config/internal/controllers/collections/alerts/get_alert_uid.go
@@ -0,0 +1,50 @@
+package alerts
+
+import (
+	"API_Config/internal/models"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gofrs/uuid"
+	"github.com/sirupsen/logrus"
+)
+
+func GetAlertUid(w http.ResponseWriter, r *http.Request) {
+	ucaIdParam := chi.URLParam(r, "uid")
+	ucaId, err := uuid.FromString(ucaIdParam)
+	if err != nil {
+		logrus.Errorf("parsing error : %s", err.Error())
+		customError := &models.CustomError{
+			Message: fmt.Sprintf("cannot parse uid (%s) as UUID", ucaIdParam),
+			Code:    http.StatusUnprocessableEntity,
+		}
+		w.WriteHeader(customError.Code)
+		body, _ := json.Marshal(customError)
+		_, _ = w.Write(body)
+		return
+	}
+
+	alert, err := alert_service.GetAlertsByResource(ucaId)
+	if err != nil {
+		logrus.Errorf("error : %s", err.Error())
+		customError, isCustom := err.(*models.CustomError)
+		if isCustom {
+			w.WriteHeader(customError.Code)
+			body, _ := json.Marshal(customError)
+			_, _ = w.Write(body)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			body, _ := json.Marshal(models.CustomError{
+				Message: "An internal server error occurred.",
+				Code:    http.StatusInternalServerError,
+			})
+			_, _ = w.Write(body)
+		}
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	body, _ := json.Marshal(alert)
+	_, _ = w.Write(body)
+}
