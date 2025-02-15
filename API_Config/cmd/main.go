051f286c0b8968b9ea45b7a44be9bfd56deb63a9
Mowsoon <alban.munari@gmail.com>
2025-02-15 16:48:23 +0100

    cmd main adding init

--- API_Config/cmd/main.go
+++ API_Config/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"API_Config/internal/controllers/alerts"
 	"API_Config/internal/controllers/resources"
+	"API_Timetable/internal/helpers"
 	"net/http"
 
 	"github.com/sirupsen/logrus"
@@ -39,4 +40,38 @@ func main() {
 	logrus.Info("[INFO] Web server started. Now listening on *:8080")
 	logrus.Fatalln(http.ListenAndServe(":8080", r))
 
+}
+
+func init() {
+	db, err := helpers.OpenDB()
+	if err != nil {
+		logrus.Fatalf("error while opening database : %s", err.Error())
+	}
+	schemes := []string{
+		`CREATE TABLE IF NOT EXISTS ressources (
+    		name TEXT NOT NULL,
+    		uid VARCHAR(255) NOT NULL,
+			id UUID PRIMARY KEY NOT NULL UNIQUE
+    	);`,
+	}
+	for _, scheme := range schemes {
+		if _, err := db.Exec(scheme); err != nil {
+			logrus.Fatalln("Could not generate table resources ! Error was : " + err.Error())
+		}
+	}
+
+	schemes = []string{
+		`CREATE TABLE IF NOT EXISTS alerts (
+    		email VARCHAR(255) NOT NULL,
+    		targets TEXT NOT NULL,
+			id UUID PRIMARY KEY NOT NULL UNIQUE
+    	);`,
+	}
+	for _, scheme := range schemes {
+		if _, err := db.Exec(scheme); err != nil {
+			logrus.Fatalln("Could not generate table alerts ! Error was : " + err.Error())
+		}
+	}
+
+	helpers.CloseDB(db)
 }
