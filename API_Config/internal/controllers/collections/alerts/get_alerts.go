36bb15e65cf3e36d8b413c112d52dc3bb80a5f11
Mowsoon <alban.munari@gmail.com>
2025-02-15 15:52:59 +0100

    controllers get_alerts.go

---
+++ API_Config/internal/controllers/collections/alerts/get_alerts.go
@@ -0,0 +1,30 @@
+package alerts
+
+import (
+	"API_Config/internal/models"
+	"encoding/json"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+)
+
+func GetAlerts(w http.ResponseWriter, _ *http.Request) {
+	// calling service
+	alerts, err := alert_service.GetAllAlerts()
+	if err != nil {
+		logrus.Errorf("error : %s", err.Error())
+		customError, isCustom := err.(*models.CustomError)
+		if isCustom {
+			w.WriteHeader(customError.Code)
+			body, _ := json.Marshal(customError)
+			_, _ = w.Write(body)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	body, _ := json.Marshal(alerts)
+	_, _ = w.Write(body)
+}
