987d48eb0d81bb66d6b3bba7a946fbc15b6fbfd2
Mowsoon <alban.munari@gmail.com>
2025-02-15 16:14:17 +0100

    controllers get_resources.go

---
+++ API_Config/internal/controllers/collections/resources/get_resources.go
@@ -0,0 +1,30 @@
+package resources
+
+import (
+	"API_Config/internal/models"
+	"API_Config/internal/services/collections/resource_service"
+	"encoding/json"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+)
+
+func GetAlerts(w http.ResponseWriter, _ *http.Request) {
+	resources, err := resource_service.GetAllResources()
+	if err != nil {
+		logrus.Errorf("error : %s", err.Error())
+		customError, isCustom := err.(*models.CustomError)
+		if isCustom {
+			w.WriteHeader(customError.Code)
+			body, _ := json.Marshal(customError)
+			_, _ = w.Write(body)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	body, _ := json.Marshal(resources)
+	_, _ = w.Write(body)
+}
