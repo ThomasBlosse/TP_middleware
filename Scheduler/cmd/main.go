beb303f0bbe99b41509991d98b8422cee3b0ef0f
Mowsoon <alban.munari@gmail.com>
2025-02-16 21:21:17 +0100

    correction of the get and adding verification of the http code

--- Scheduler/cmd/main.go
+++ Scheduler/cmd/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"github.com/sirupsen/logrus"
 	"io"
 	"net/http"
 )
 
 func main() {
-	resp, err := http.Get("//localhost:8080/resources")
+	resp, err := http.Get("http://localhost:8080/resources")
 	if err != nil {
 		logrus.Fatalf("Error while fetching resources: %s", err.Error())
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		logrus.Fatalf("Unexpected status code: %d - Response: %s", resp.StatusCode, string(body))
+	}
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
