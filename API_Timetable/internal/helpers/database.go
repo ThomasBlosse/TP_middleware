c748a3ff452e4e772f4511bef75b5ce815f8fe31
mowsoon <alban.munari@gmail.com>
2025-03-11 17:36:07 +0100

    removing useless function in helpers

--- API_Timetable/internal/helpers/database.go
+++ API_Timetable/internal/helpers/database.go
@@ -3,9 +3,6 @@ package helpers
 import (
 	"database/sql"
 	"fmt"
-	"strings"
-
-	"github.com/gofrs/uuid"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/sirupsen/logrus"
 )
@@ -24,33 +21,4 @@ func CloseDB(db *sql.DB) {
 	if err != nil {
 		logrus.Errorf("error closing db : %s", err.Error())
 	}
-}
-
-func UUIDSliceToString(uuids []*uuid.UUID) string {
-
-	var strUUIDs []string
-
-	for _, u := range uuids {
-
-		strUUIDs = append(strUUIDs, u.String())
-
-	}
-
-	return strings.Join(strUUIDs, ",")
-
-}
-
-func StringToUUIDSlice(s string) ([]*uuid.UUID, error) {
-	parts := strings.Split(s, ",")
-	var uuids []*uuid.UUID
-
-	for _, part := range parts {
-		part = strings.TrimSpace(part)
-		uuid, err := uuid.FromString(part)
-		if err != nil {
-			return nil, err
-		}
-		uuids = append(uuids, &uuid)
-	}
-	return uuids, nil
 }
