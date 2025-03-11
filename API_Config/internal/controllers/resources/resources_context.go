bf556963763551379ca46d53b71e041c10350560
mowsoon <alban.munari@gmail.com>
2025-03-11 19:31:12 +0100

    updating resources_context.go in order to have the uid in the integer format

--- API_Config/internal/controllers/resources/resources_context.go
+++ API_Config/internal/controllers/resources/resources_context.go
@@ -5,20 +5,19 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"net/http"
-
 	"github.com/go-chi/chi/v5"
-	"github.com/gofrs/uuid"
 	"github.com/sirupsen/logrus"
+	"net/http"
+	"strconv"
 )
 
 func Ctx(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		collectionId, err := uuid.FromString(chi.URLParam(r, "id"))
-		if err != nil {
-			logrus.Errorf("parsing error : %s", err.Error())
+		uidStr := chi.URLParam(r, "id")
+		if uidStr == "" {
+			logrus.Errorf("parsing error : uidStr empty")
 			customError := &models.CustomError{
-				Message: fmt.Sprintf("cannot parse id (%s) as UUID", chi.URLParam(r, "id")),
+				Message: "cannot parse uidStr, it is empty",
 				Code:    http.StatusUnprocessableEntity,
 			}
 			w.WriteHeader(customError.Code)
@@ -27,7 +26,20 @@ func Ctx(next http.Handler) http.Handler {
 			return
 		}
 
-		ctx := context.WithValue(r.Context(), "ResourcesId", collectionId)
+		uid, err := strconv.Atoi(uidStr)
+		if err != nil {
+			logrus.Errorf("parsing error: %s", err.Error())
+			customError := &models.CustomError{
+				Message: fmt.Sprintf("cannot parse uid (%s) to int", uidStr),
+				Code:    http.StatusUnprocessableEntity,
+			}
+			w.WriteHeader(customError.Code)
+			body, _ := json.Marshal(customError)
+			_, _ = w.Write(body)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "resourceId", uid)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
