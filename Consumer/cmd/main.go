d79df6722df38fd9be2e5ea0b0bdc00ad8882ca6
mowsoon <alban.munari@gmail.com>
2025-03-13 15:33:38 +0100

    refactoring main

--- Consumer/cmd/main.go
+++ Consumer/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"Consumer/internal/helpers"
 	"Consumer/internal/models"
 	"encoding/json"
 	"github.com/sirupsen/logrus"
@@ -12,26 +13,6 @@ func main() {
 	var collections []models.Collection
 	//TODO consume collections from mq
 
-	for _, collection := range collections {
-		query := "http://localhost:8080/collections/" + collection.Uid
-		resp, err := http.Get(query)
-		if err != nil {
-			logrus.Fatalf("Error while fetching collection: %s", err.Error())
-		}
-		defer resp.Body.Close()
-		if resp.StatusCode != http.StatusOK {
-			body, _ := io.ReadAll(resp.Body)
-			logrus.Fatalf("Unexpected status code: %d - Response: %s", resp.StatusCode, string(body))
-		}
-		var body []byte
-		body, err = io.ReadAll(resp.Body)
-		if err != nil {
-			logrus.Fatalf("Error while reading collection: %s", err.Error())
-		}
-		var timetable models.Collection
-		if err := json.Unmarshal(body, &timetable); err != nil {
-			logrus.Fatalf("Error while unmarshalling resources: %s", err.Error())
-		}
-
-	}
+	notifications := helpers.GeneratingNotification(collections)
+	//TODO send notifiation to alerter
 }
