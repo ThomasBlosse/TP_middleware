5fa6959457ef2c8e15df550309e5a82f443b2b21
mowsoon <alban.munari@gmail.com>
2025-04-04 12:50:58 +0200

    alerter implementing the function that send mails

--- Alerter/internal/helpers/mail.go
+++ Alerter/internal/helpers/mail.go
@@ -2,18 +2,29 @@ package helpers
 
 import (
 	"Alerter/internal/models"
+	"bytes"
 	"embed"
 	"encoding/json"
+	"fmt"
+	"github.com/adrg/frontmatter"
 	"github.com/sirupsen/logrus"
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
+	"text/template"
 )
 
 var embeddedTemplates embed.FS
 
 func SendMail(notification models.Notification) {
 	alerts := getAlerts(notification.ResourceIds)
+	for _, alert := range alerts {
+		err := writeMail(alert.Email, notification.Description, notification.OldValue, notification.NewValue)
+		if err != nil {
+			logrus.Error(err)
+		}
+	}
 }
 
 func getAlerts(ResourceIds []int) []models.Alerts {
@@ -50,6 +61,69 @@ func getAlerts(ResourceIds []int) []models.Alerts {
 	return allAlerts
 }
 
-func writeMail(email string, description string, base string, change string) {
+func writeMail(email string, description string, base string, change string) error {
+	mailContent, mailSubject, err := getStringFromEmbeddedTemplate("templates/email.html", models.MailTemplateData{
+		EventName:   "Modification d'un événement",
+		Description: description,
+		Base:        base,
+		Change:      change,
+	})
+	if err != nil {
+		logrus.Fatalf("Error parsing email template: %s", err.Error())
+	}
 
+	mailReq := models.MailRequest{
+		Recipient: email,
+		Subject:   mailSubject,
+		Content:   mailContent,
+	}
+	reqBody, err := json.Marshal(mailReq)
+	if err != nil {
+		return fmt.Errorf("error marshalling mail request: %w", err)
+	}
+
+	req, err := http.NewRequest("POST", "https://mail-api.edu.forestier.re/mail", bytes.NewBuffer(reqBody))
+	if err != nil {
+		return fmt.Errorf("error creating HTTP request: %w", err)
+	}
+
+	req.Header.Set("Authorization", "token") //Todo hiding token
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error sending mail: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// Lire la réponse
+	body, _ := io.ReadAll(resp.Body)
+	if resp.StatusCode != http.StatusNoContent {
+		return fmt.Errorf("failed to send email, status: %d, response: %s", resp.StatusCode, string(body))
+	}
+
+	logrus.Infof("Email successfully sent to %s", email)
+	return nil
+}
+
+func getStringFromEmbeddedTemplate(templatePath string, body interface{}) (content string, subject string, err error) {
+	temp, err := template.ParseFS(embeddedTemplates, templatePath)
+	if err != nil {
+		return "", "", err
+	}
+	var tpl bytes.Buffer
+	if err := temp.Execute(&tpl, body); err != nil {
+		return "", "", err
+	}
+
+	var matter struct {
+		Subject string `yaml:"subject"`
+	}
+	mailContent, err := frontmatter.Parse(strings.NewReader(tpl.String()), &matter)
+	if err != nil {
+		return "", "", err
+	}
+
+	return string(mailContent), matter.Subject, nil
 }
