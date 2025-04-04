f77c3fdc6667799b0d326f3c176b9ba7b16bbabb
mowsoon <alban.munari@gmail.com>
2025-04-04 11:56:41 +0200

    alerter correcting first for

--- Alerter/internal/helpers/mail.go
+++ Alerter/internal/helpers/mail.go
@@ -17,7 +17,7 @@ func getAlerts(ResourceIds []int) []models.Alerts {
 	var allAlerts []models.Alerts
 	alertsMap := make(map[string]struct{})
 
-	for resourceId := range ResourceIds {
+	for _, resourceId := range ResourceIds {
 		resp, err := http.Get("http://localhost:8081/alerts/" + strconv.Itoa(resourceId))
 		if err != nil {
 			logrus.Fatalf("Error while fetching alerts: %s", err.Error())
